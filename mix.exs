a980492d2398f998613ebfa9382d7d03717a10fa
nelsonic <nelson+github@dwyl.com>
2023-05-04 23:14:15 +0100

    create package info #1

--- mix.exs
+++ mix.exs
@@ -4,19 +4,21 @@ defmodule Gcal.MixProject do
   def project do
     [
       app: :gcal,
-      version: "0.1.0",
-      elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      test_coverage: [tool: ExCoveralls],
+      description: description(),
+      elixir: "~> 1.14",
+      package: package(),
       preferred_cli_env: [
         c: :test,
         coveralls: :test,
         "coveralls.json": :test,
         "coveralls.html": :test,
         t: :test
-      ]
+      ],
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      version: "1.0.0"
     ]
   end
 
@@ -27,21 +29,41 @@ defmodule Gcal.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-
-      # create docs on localhost by running "mix docs"
-      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
-      # Track test coverage: github.com/parroty/excoveralls
-      {:excoveralls, "~> 0.15", only: [:test, :dev]},
-    ]
-  end
-
   defp aliases do
     [
       t: ["test"],
       c: ["coveralls.html"]
+    ]
+  end
+
+  defp description() do
+    "The easy way to interact with Google Calendar from your Elixir Apps"
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # HTTP request lib
+      {:httpoison, "~> 2.0"},
+
+      # Datetime transformation
+      {:timex, "~> 3.0"},
+
+      # Create docs on localhost by running "mix docs"
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
+
+      # Track test coverage: github.com/parroty/excoveralls
+      {:excoveralls, "~> 0.15", only: [:test, :dev]}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ["lib/gcal.ex", "mix.exs", "README.md"],
+      name: "gcal",
+      licenses: ["GNU GPL v2.0"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/dwyl/gcal"}
     ]
   end
 end
