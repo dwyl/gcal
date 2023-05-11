aeae4add7e7fd4abad2ffa6d9632d3bc1c09e8d6
nelsonic <nelson+github@dwyl.com>
2023-05-11 06:29:23 +0100

    create /config for http mocking #1

---
+++ config/test.exs
@@ -0,0 +1,12 @@
+import Mix.Config
+
+config :gcal,
+  # client_id: "631770888008-6n0oruvsm16kbkqg6u76p5cv5kfkcekt.apps.googleusercontent.com",
+  # client_secret: "MHxv6-RGF5nheXnxh1b0LNDq",
+  httpoison_mock: true
+  # httpoison_mock: false
+
+# System.put_env(
+#   "GOOGLE_CLIENT_ID",
+#   "631770888008-6n0oruvsm16kbkqg6u76p5cv5kfkcekt.apps.googleusercontent.com"
+# )
